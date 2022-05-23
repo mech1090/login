10fc41a2bf260e6e1d394124c6f5e3cddd2c6a11
Shahzad <mech1090@gmail.com>
2022-05-23 09:54:05 -0600

    login and signup page login done

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,11 +1,16 @@
 const modelUser = require('../model/user')
 const bcrypt = require('bcrypt')
+const config = require('config')
 const getLoginForm = (req,res)=>{
     res.render('login/layout')
 }
 const login = async(req,res)=>{
     const {email,password} = req.body
-    console.log(email,password)
+    const findUser = await modelUser.findOne({email})
+    const matchPassword = await bcrypt.compare(password,findUser.password)
+    if(!matchPassword){
+        return res.render('login/layout',{message:'Email or Password Wrong'})
+    }
 
     return res.render('login/layout',{message:'login successfull'})
 
@@ -14,8 +19,15 @@ const login = async(req,res)=>{
 const getSignForm = (req,res)=>{
     res.render('signup/layout')
 }
-const signup = (req,res)=>{
-
+const signup =async (req,res)=>{
+    const {email,password} = req.body
+    const emailFind = await modelUser.findOne({email})
+    if(emailFind){
+        return res.render('signup/layout',{message:'User Already exist'})
+    }
+    const hashedPassword = await bcrypt.hash(password,config.get('hashing.salt'))
+    const newUser = await modelUser.create({email,password:hashedPassword})
+    return res.render('signup/layout',{message:'New User Created'})
 }
 
 module.exports = {getLoginForm,login,getSignForm,signup}
